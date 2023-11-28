7568a7a21db5a1d797e3a6b42bfd5267f13d898c
edintech23 <preteledwin23@gmail.com>
2023-11-28 16:57:26 -0600

    Challenge02

---
+++ OpsChallenge:Class02.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# defining source and destination filenames
+src_file="/var/log/syslog"
+current=$(date +"%Y-%m-%d-%H:%M:%S")
+dest_file="/home/edintech/Desktop/syslog-$current"
+
+# copy syslog file to current working directory with timestamp
+cp $src_file $dest_file
+
+echo "Log file copied to: $dest_file"
