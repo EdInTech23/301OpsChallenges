6f94fad6fe58d31c8585919d913aa7e97bf44b0c
edintech23 <preteledwin23@gmail.com>
2023-12-04 14:07:07 -0600

    k

---
+++ Pythonintro.py
@@ -0,0 +1,25 @@
+# Author:      Edwin Pretel
+# Script:      Ops 301 chall 06
+# Purpose
+# Why:
+
+# Variables 
+#user = 'Roger'
+ 
+ # input
+#print(user)
+#user = input('please enter your name: ')
+ # output
+#print ('There is going to be a user name printed: ' + user)
+#print(user)
+ # Imports 
+import os 
+ # Bash Things in Python
+# os.system('ls -la')
+ # Function
+def my_function():
+     os.system('ls -la')
+     os.system('ip a')
+     
+my_function()
+ # Stretch 
