fbe2261e0e024460b3b68ebe09c8616b357ae615
Edwin Pretel <147413178+EdInTech23@users.noreply.github.com>
2023-12-06 22:39:12 -0500

    Create OpsChallenge:Class08.py

---
+++ OpsChallenge:Class08.py
@@ -0,0 +1,14 @@
+# Create a list of ten string elements
+fruits = ["apple", "banana", "cherry", "orange", "mango", "grapefruit", "kiwi", "pineapple", "watermelon", "strawberry"]
+
+# Print the fourth element of the list
+print("Fourth element:", fruits[3])
+
+# Print the sixth through tenth element of the list
+print("Sixth and later elements:", fruits[5:])
+
+# Change the value of the seventh element to "onion"
+fruits[6] = "onion"
+
+# Print the updated list
+print("Updated list:", fruits)
