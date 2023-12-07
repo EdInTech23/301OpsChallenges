5d4503ec02aad0740bbf0790e94967c0d2df4efa
Edwin Pretel <147413178+EdInTech23@users.noreply.github.com>
2023-12-07 18:26:17 -0500

    Create OpsChallenge:Class09.py

---
+++ OpsChallenge:Class09.py
@@ -0,0 +1,19 @@
+# Dog sizes
+height = 24  # inches
+weight = 45  # pounds
+
+# Check if the dog is small
+if height <= 12 and weight <= 25:
+    print("The dog is small.")
+
+# Check if the dog is medium
+elif height <= 18 and weight <= 50:
+    print("The dog is medium.")
+
+# Check if the dog is large
+elif height <= 24 and weight <= 75:
+    print("The dog is large.")
+
+# Check if the dog is giant
+else:
+    print("The dog is giant.")
